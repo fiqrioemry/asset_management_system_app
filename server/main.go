d786fe65f0ff4ba7f369c70830b6a68abb7a2431
fiqrioemry <foemry@gmail.com>
2025-07-10 21:24:52 +0700

    fix configuration middleware, add health endpoitn check

--- server/main.go
+++ server/main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"log"
+
+	"github.com/fiqrioemry/asset_management_system_app/server/config"
+	"github.com/fiqrioemry/asset_management_system_app/server/handlers"
+	"github.com/fiqrioemry/asset_management_system_app/server/middlewares"
+	"github.com/fiqrioemry/asset_management_system_app/server/repositories"
+	"github.com/fiqrioemry/asset_management_system_app/server/routes"
+	"github.com/fiqrioemry/asset_management_system_app/server/seeders"
+	"github.com/fiqrioemry/asset_management_system_app/server/services"
+	"github.com/fiqrioemry/asset_management_system_app/server/utils"
+
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+)
+
+// ASSET MANAGEMENT APP SERVER
+// VERSION: 1.0.0
+// DEPLOYMENT: docker-compose
+// PORT: 5005
+// DESCRIPTION: This is a server for an asset management system that handles user registration, asset management, and payment processing.
+
+func main() {
+	// ========== Configuration =================
+	config.InitConfiguration()
+	utils.InitLogger()
+	db := config.DB
+
+	seeders.ResetDatabase(db)
+
+	// ========== initialisasi layer ============
+	repo := repositories.InitRepositories(db)
+	s := services.InitServices(repo)
+	h := handlers.InitHandlers(s)
+
+	// ========== Inisialisasi gin engine =======
+	r := gin.Default()
+	r.SetTrustedProxies(config.AppConfig.TrustedProxies)
+
+	// ========== inisialisasi Middleware ========
+	r.Use(
+		ginzap.Ginzap(utils.GetLogger(), time.RFC3339, true),
+		middlewares.Recovery(),
+		middlewares.CORS(),
+		middlewares.RateLimiterInit(),
+		middlewares.LimitFileSize(config.AppConfig.MaxFileSize),
+		middlewares.APIKeyGateway(config.AppConfig.SkippedApiEndpoints),
+	)
+
+	// ========== inisialisasi routes ===========
+	routes.InitRoutes(r, h)
+
+	port := config.AppConfig.ServerPort
+	log.Println("server running on port:", port)
+	log.Fatal(r.Run(":" + port))
+}
